3e5b6d74717eba459887dfeed6bc21a7694cef91
caroljoyv <caroljoyv27@gmail.com>
2023-05-17 01:09:10 +0530

    added validation fort the post method using pydantic

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title : str
+    content : str
 
 
 @app.get("/")           # path operation
@@ -13,6 +18,7 @@ def get_post():
     return {"data":"This is your post"}
 
 @app.post("/createposts")
-def create_post(payload: dict = Body(...)):
-    print(payload)
-    return {"newpost":f"title: {payload['title']} content:{payload['content']}"}
+def create_post(new_post: Post):
+    print(new_post)
+    print(new_post.title)
+    return {"data":"new_post recieved"}
