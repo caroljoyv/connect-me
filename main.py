bd6bee16497b1b06b4b154949735d9a8b7bc499d
caroljoyv <caroljoyv27@gmail.com>
2023-05-18 21:42:37 +0530

    added functions to retrieve a post by a its id

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
@@ -15,16 +16,32 @@ class Post(BaseModel):
 my_post = [{"title": "title of post 1","content":"content of post 1","id":1},{"title":"favourite food","content":"I love shawarma","id":2}]
 
 
+def find_post(id):      #function to find a post with a certain id
+    for p in my_post:
+        if p['id'] == id:
+            return p
+
 
 @app.get("/")           # path operation
 def root():
     return {"message": "Welcome to my api peeps!"}
 
-@app.get("/posts")
+@app.get("/posts")     #get method
 def get_post():
     return {"data":my_post}
 
-@app.post("/posts")
-def create_post(post: Post):
-    print(post.rating)
-    return {"data":"post recieved"}
+@app.post("/posts")    #post method
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0,10000)
+    my_post.append(post_dict)
+    return {"data":post_dict }
+    
+#retrieving a post by id
+@app.get("/posts/{id}")
+def get_posts(id):
+    post = find_post(int(id))
+    return {"Post details ": post}
+
+
+
